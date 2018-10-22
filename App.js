93ee64e33da055fbfced44abff3619c782b5fba0
Mario <kowalewski.mariusz@gmail.com>
2018-10-22 19:11:59 +0200

    feat: Create NewDeck and Decks components

--- App.js
+++ App.js
@@ -1,5 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation'
+
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+import Decks from './components/Decks'
+import NewDeck from './components/NewDeck'
+
+const Tabs = createMaterialTopTabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    //activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      //backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {

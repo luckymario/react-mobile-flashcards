bf454095d174e38471588db9a789b32f9eae3339
Mario <kowalewski.mariusz@gmail.com>
2018-10-26 20:27:03 +0200

    feat: Add data file

--- utils/api.js
+++ utils/api.js
@@ -1,29 +1,5 @@
 import { AsyncStorage } from 'react-native'
-
-const decks = {
-  React: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
-      },
-      {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
-  },
-  JavaScript: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  }
-}
+import { decks } from './_DATA'
 
 AsyncStorage.setItem('decks', JSON.stringify(decks))
 

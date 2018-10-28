6a104f515c5a3f5c9114b85ee7398efed3b6f3c3
Mario <kowalewski.mariusz@gmail.com>
2018-10-28 19:31:21 +0100

    style: Remove comments

--- utils/api.js
+++ utils/api.js
@@ -1,10 +1,10 @@
 import { AsyncStorage } from 'react-native'
 import { decks } from './_DATA'
 
+// Set initial data
 //AsyncStorage.setItem('decks', JSON.stringify(decks))
 //AsyncStorage.removeItem('decks')
 
-// Set initial data
 /*AsyncStorage.getItem('decks')
   .then(JSON.parse)
   .then((data) => {
@@ -13,27 +13,23 @@ import { decks } from './_DATA'
     }
   })*/
 
-// getDecks: return all of the decks along with their titles, questions, and answers.
 export function getDecks () {
 	return AsyncStorage.getItem('decks')
 		.then((results) => JSON.parse(results))
 }
 
-// getDeck: take in a single id argument and return the deck associated with that id.
 export function getDeck (id) {
 	return AsyncStorage.getItem('decks')
 		.then((results) => JSON.parse(results))
 		.then((data) => data[id])
 }
 
-// saveDeckTitle: take in a single title argument and add it to the decks.
 export function saveDeck (id, deck) {
 	return AsyncStorage.mergeItem('decks', JSON.stringify({
 		[id]: deck,
 	}))
 }
 
-// addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
 export function addCardToDeck (deckId, card) {
 	return AsyncStorage.getItem('decks')
 		.then((results) => {
